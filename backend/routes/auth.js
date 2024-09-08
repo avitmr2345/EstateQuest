59303bde30886aced4aed91937a7c2437294556c
Avish <avitmr2345@gmail.com>
2024-09-08 20:42:43 +0530

    sign out functionality added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup, signin, google } from "../controllers/auth.js";
+import { signup, signin, signOut, google } from "../controllers/auth.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.get("/signout", signOut);
 router.post("/google", google);
 
 export default router;
