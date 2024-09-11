eda1c2bc871823c474177932b4775d09ab688d0d
Avish <avitmr2345@gmail.com>
2024-09-11 23:54:25 +0530

    created get user listings api route  main

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.js";
+import Listing from "../models/listing.js";
 import { errorHandler } from "../utils/error.js";
 
 export const updateUser = async (req, res, next) => {
@@ -40,5 +41,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { updateUser, deleteUser } from "../controllers/user.js";
+import {
+  updateUser,
+  deleteUser,
+  getUserListings,
+} from "../controllers/user.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
