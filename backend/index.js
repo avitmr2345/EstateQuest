8af075f6eb796d9997d5a4a17195af288f0bcb83
Avish <avitmr2345@gmail.com>
2024-09-15 20:34:30 +0530

    added pre installation

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ import authRoute from "./routes/auth.js";
 import userRoute from "./routes/user.js";
 import listingRoute from "./routes/listing.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 const app = express();
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO)
@@ -20,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/listing", listingRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   //middleware for handling error
